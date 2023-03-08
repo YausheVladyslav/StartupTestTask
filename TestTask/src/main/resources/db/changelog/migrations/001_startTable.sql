17201a98080fad6acc1e40f9f3182b0ef506a0ac
YaushevVladyslav <i.vlad.yaushev@gmail.com>
2023-03-08 11:09:31 +0200

    change varchar and delete comment

--- TestTask/src/main/resources/db/changelog/migrations/001_startTable.sql
+++ TestTask/src/main/resources/db/changelog/migrations/001_startTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE person
 (
     id      BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
+    name    VARCHAR(64) NOT NULL,
+    surname VARCHAR(64) NOT NULL,
     birthday    DATE         NOT NULL
 );
